572d09ab7d561d65aa4d41a744b9f58a73c16938
EvoX7 <creativesoultouch@gmail.com>
2022-09-13 16:41:24 +0200

    Init

---
+++ app/Http/Controllers/comicController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Comic;
+use Illuminate\Http\Request;
+
+class comicController extends Controller
+{
+    public function index()
+    {
+        $comics = Comic::all();
+
+        return view('index', compact('comics'));
+    }
+}
---
+++ resources/views/index.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.main_template')
+
+
+@section('comics-table')
+<div class="container">
+    <div class="row">
+        <div class="col-12">
+            
+        </div>
+    </div>
+</div>
+@endsection
